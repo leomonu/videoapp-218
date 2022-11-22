d976887365e5617db20f7496588dc04dad2725c8
leomonu <pratheerthk@gmail.com>
2022-11-22 20:49:09 +0530

    second commit

---
+++ README.md
@@ -0,0 +1,4 @@
+npm install npm -g
+npm install --global yarn
+
+cors - cross origin resource sharing
---
+++ public/script.js
@@ -0,0 +1,44 @@
+const socket = io("/")
+var peer = new Peer(undefined,{
+  path:"/peerjs",
+  host:"/",
+  port:"443"
+
+})
+const  user = prompt("Enter Your Name")
+$(function () {
+    $("#show_chat").click(function () {
+      $(".left-window").css("display", "none");
+      $(".right-window").css("display", "block");
+      $(".header_back").css("display", "block");
+    });
+    $(".header_back").click(function () {
+        $(".left-window").css("display", "block");
+        $(".right-window").css("display", "none");
+        $(".header_back").css("display", "none");
+      })
+$("#send").click(function(){
+    if($("#chat_message").val().length!==0){
+        socket.emit("message",$("chat_message").val())
+        $("chat_message").val()="f"
+    }
+})
+  });
+
+  peer.on("open",(id)=>{
+    socket.emit("join-room",ROOM_ID,id,user)
+  })
+  socket.on("createMessage",(message,userName)=>{
+    $(".messages").append(`
+      <div class = "message">
+        <b>
+          <i class = "far fa-user-circle"></i> <span>${
+            userName === user?"me":userName
+          }</span>
+        </b>
+        <span>${message}</span>
+      </div>
+    `)
+  })
+
+  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
